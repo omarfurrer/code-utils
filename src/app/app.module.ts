cff8661d621a148ca9aead343d773f4bfef501de
Omar Furrer <omar.furrer@gmail.com>
2018-03-06 22:39:08 +0200

    Add main content component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
+import { ContentComponent } from './content/content.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SideBarComponent
+    SideBarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
